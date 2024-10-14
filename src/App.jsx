0cab33b7308182df18018a2f72ae99fc173ff8b5
Olamilekan Kareem <olamilekankareem717@gmail.com>
2024-10-14 15:28:18 +0100

    updated paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 import './App.css'
 import 'tailwindcss/tailwind.css';
 import './output.css'
-
+import Login from './Pages/Login';
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-          <h1 className="text-center text-white text-3xl font-bold underline bg-blue-500">Hello World!!</h1>
+        <Login/>
     </>
   )
 }
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,16 @@
+
+import './../App.css'
+import 'tailwindcss/tailwind.css';
+import './../output.css'
+
+function Login() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+          <h1 className="text-center text-white text-3xl font-bold underline bg-blue-500">Hello World!!</h1>
+    </>
+  )
+}
+
+export default Login
